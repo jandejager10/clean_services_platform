787de18c8ce71fdf33c03b0d38989dd7b01706d2
jandejager10 <40761225+jandejager10@users.noreply.github.com>
2024-10-09 21:28:59 +0100

    product links, views, models, templates updated

--- products/views.py
+++ products/views.py
@@ -42,9 +42,8 @@ def all_products(request):
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
-                messages.error(request,
-                               ("You didn't enter any search criteria!"))
-                return redirect(reverse('products'))
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('products:product_list'))
 
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
@@ -85,11 +84,9 @@ def add_product(request):
         if form.is_valid():
             product = form.save()
             messages.success(request, 'Successfully added product!')
-            return redirect(reverse('product_detail', args=[product.id]))
+            return redirect(reverse('products:product_detail', args=[product.id]))
         else:
-            messages.error(request,
-                           ('Failed to add product. '
-                            'Please ensure the form is valid.'))
+            messages.error(request, 'Failed to add product. Please ensure the form is valid.')
     else:
         form = ProductForm()
 
@@ -114,11 +111,9 @@ def edit_product(request, product_id):
         if form.is_valid():
             form.save()
             messages.success(request, 'Successfully updated product!')
-            return redirect(reverse('product_detail', args=[product.id]))
+            return redirect(reverse('products:product_detail', args=[product.id]))
         else:
-            messages.error(request,
-                           ('Failed to update product. '
-                            'Please ensure the form is valid.'))
+            messages.error(request, 'Failed to update product. Please ensure the form is valid.')
     else:
         form = ProductForm(instance=product)
         messages.info(request, f'You are editing {product.name}')
@@ -142,4 +137,4 @@ def delete_product(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
     product.delete()
     messages.success(request, 'Product deleted!')
-    return redirect(reverse('products'))
+    return redirect(reverse('products:product_list'))
--- services/views.py
+++ services/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Service
 
+
 # Create your views here.
 def service_list(request):
     return render(request, 'services/service_list.html')
