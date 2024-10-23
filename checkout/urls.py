5a9c9cb17801b8aea6ea7e39b4ad26b822ccad73
jandejager10 <40761225+jandejager10@users.noreply.github.com>
2024-10-23 09:43:03 +0100

    checkout URLs update

--- checkout/urls.py
+++ checkout/urls.py
@@ -6,6 +6,6 @@ app_name = 'checkout'
 
 urlpatterns = [
     path('', views.checkout, name='checkout'),
-    path('success/<order_number>', views.checkout_success, name='checkout_success'),
+    path('success/<str:order_number>', views.checkout_success, name='checkout_success'),
     path('wh/', webhook, name='webhook'),
 ]
--- checkout/views.py
+++ checkout/views.py
@@ -89,7 +89,7 @@ def checkout(request):
                     return redirect(reverse('view_cart'))
             # Save the info to the user's profile if all is well
             request.session['save_info'] = 'save-info' in request.POST
-            return redirect(reverse('checkout_success',
+            return redirect(reverse('checkout:checkout_success',
                                     args=[order.order_number]))
         else:
             messages.error(request, ('There was an error with your form. '
