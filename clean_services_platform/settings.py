dbe9b448df7a333b1d59f7acae2f386c838ee320
jandejager10 <40761225+jandejager10@users.noreply.github.com>
2024-12-06 12:50:34 +0000

    Reset migrations and update database config

--- clean_services_platform/settings.py
+++ clean_services_platform/settings.py
@@ -206,13 +206,18 @@ STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 # Database
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+        'default': dj_database_url.config(
+            default=os.environ.get('DATABASE_URL'),
+            conn_max_age=600,
+            conn_health_checks=True,
+            ssl_require=True
+        )
     }
 else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+            'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
 
