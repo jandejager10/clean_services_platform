d83af45f3bb11e8f7673ac7dd2e41d3d8e29823a
jandejager10 <40761225+jandejager10@users.noreply.github.com>
2024-12-06 12:16:39 +0000

    Prepare for Heroku deployment 2

---
+++ checkout/migrations/0002_alter_order_status.py
@@ -0,0 +1,26 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('checkout', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='order',
+            name='status',
+            field=models.CharField(
+                choices=[
+                    ('pending', 'Pending'),
+                    ('processing', 'Processing'),
+                    ('cancellation_requested', 'Cancellation Requested'),
+                    ('completed', 'Completed'),
+                    ('cancelled', 'Cancelled'),
+                ],
+                default='pending',
+                max_length=25,
+            ),
+        ),
+    ] 
